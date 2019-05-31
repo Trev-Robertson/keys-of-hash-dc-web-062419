3e7a87c20c9eb4db31aa8bf4631de617ac6b7633
Trevahr Robertson <trevahr@yahoo.com>
2019-05-31 02:22:57 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,19 @@
 class Hash
 
 
+<<<<<<< HEAD
   def keys_of(*arguments)
    map {|key, value| arguments.include?(value) ? key : nil }.compact
+=======
+ 
+    
+  def keys_of(*args)
+    map {|key, value| args.include?(value) ? key : nil }.compact
+>>>>>>> 22968b879da855f2aa3aaa1c6f7d79f66aacae91
   end
+    
+    
+
+
+
 end
